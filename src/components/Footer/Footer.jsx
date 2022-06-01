db0cbb89acc1d33c6ea7d35f1d237c9190d48a94
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-01 18:46:58 +0800

    setup footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import "./Footer.scss";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>©2021, Movie, Inc. or its affiliates</div>
+    </div>
+  );
 }
