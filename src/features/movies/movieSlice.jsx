f4573d2e68e5ef2be84c481735d06f9b82674848
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-02 23:41:13 +0800

    setup fetch async movie and show details

--- src/features/movies/movieSlice.jsx
+++ src/features/movies/movieSlice.jsx
@@ -24,17 +24,26 @@ export const fetchAsyncShows = createAsyncThunk(
   }
 );
 
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk(
+  "movies/fetchAsyncMovieOrShowDetail",
+  async (id) => {
+    const response = await movieApi.get(`?apiKey=${APIKey}&i=${id}&Plot=full`);
+    return response.data;
+  }
+);
+
 const initialState = {
   movies: {},
   shows: {},
+  selectMovieOrShow: {},
 };
 
 const movieSlice = createSlice({
   name: "movies",
   initialState,
   reducers: {
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
+    removeSelectedMovieOrShow: (state) => {
+      state.selectMovieOrShow = {};
     },
   },
   extraReducers: {
@@ -52,10 +61,15 @@ const movieSlice = createSlice({
       console.log("Fetched Successfully!");
       return { ...state, shows: payload };
     },
+    [fetchAsyncMovieOrShowDetail.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully!");
+      return { ...state, selectMovieOrShow: payload };
+    },
   },
 });
 
-export const { addMovies } = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMovieOrShow = (state) => state.movies.selectMovieOrShow;
 export default movieSlice.reducer;
