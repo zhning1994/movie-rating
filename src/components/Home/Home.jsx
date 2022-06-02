789184d092ad210861757dbe79f45395d6214032
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-02 22:27:43 +0800

    setup the asynthunk for fetch movies, at extraReducers part, modified what to do for different async action life circle

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,15 @@
 import React, { useEffect } from "react";
 import "./Home.scss";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 
 export default function Home() {
   const dispatch = useDispatch();
   const movieText = "Harry";
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log(err);
-        });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]);
 
   return (
     <>
