00e0722213c76075c76e639d2618f1191e0e83ac
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-02 22:00:32 +0800

    setup movie card which get the details of movie from prop

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./MovieCard.scss";
 
-export default function MovieCard() {
-  return <div>MovieCard</div>;
+export default function MovieCard(props) {
+  const { data } = props;
+  return (
+    <div className="card-item">
+      <Link to={`/movie/${data.imdbID}`}>
+        <div className="card-inner">
+          <div className="card-top">
+            <img src={data.Poster} alt={data.Title} />
+          </div>
+          <div className="card-bottom">
+            <div className="card-info">
+              <h4>{data.Title}</h4>
+              <p>{data.Year}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 }
